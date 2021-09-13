87f66d431ee256a29d78373c6bb72e5399a24758
miltank-405 <koitn.saru@gmail.com>
2021-09-13 22:45:12 +0900

    とりあえずフィズバズ書いた

--- nabeatsu.js
+++ nabeatsu.js
@@ -2,3 +2,15 @@
 // そこから応用を考えていく
 // ルール：3の倍数と3がつく時だけアホになる
 //   8の倍数と8がつくときはおもろく無いけどオモローって表示する
+
+for(var i = 1; i <= 100; i++) {
+	if(i % 15 == 0) {
+		document.write("FizzBuzz" + "<br/>");
+	} else if ((i % 3 == 0) && (i % 15 != 0)) {
+		document.write("Fizz" + "</br>");
+	} else if ((i % 5 == 0) && (i % 15 != 0)) {
+		document.write("Buzz" + "</br>");
+	} else {
+		document.write (i + "<br />");
+	}
+}
