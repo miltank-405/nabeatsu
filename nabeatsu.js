4cd8c0eb09f30c6450776efeffc6fc4e1c3958a8
miltank-405 <koitn.saru@gmail.com>
2021-09-14 12:59:13 +0900

    ==から===に変更

--- nabeatsu.js
+++ nabeatsu.js
@@ -4,10 +4,10 @@
 //   8の倍数と8がつくときはおもろく無いけどオモローって表示する
 
 for(var i = 1; i <= 100; i++) {
-	if (i % 3 == 0) {
-		document.write("アホ〜!!" + "<br/>");
-	} else if (i % 8 == 0) {
-		document.write("オーモロー!!" + "</br>");
+	if (i % 3 === 0) {
+		document.write(i + "アホ〜!!" + "<br/>");
+	} else if (i % 8 === 0) {
+		document.write(i + "オーモロー!!" + "</br>");
 	} else {
 		document.write (i + "<br/>");
 	}
