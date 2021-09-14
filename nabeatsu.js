78966997e5ce767cca7ba4ec4a5ff620e7ed8573
miltank-405 <koitn.saru@gmail.com>
2021-09-14 13:05:47 +0900

    || /3/.test(i)追加で3つく数字も反映されると思った

--- nabeatsu.js
+++ nabeatsu.js
@@ -4,7 +4,7 @@
 //   8の倍数と8がつくときはおもろく無いけどオモローって表示する
 
 for(var i = 1; i <= 100; i++) {
-	if (i % 3 === 0) {
+	if (i % 3 === 0 || /3/.test(i)) {
 		document.write(i + "アホ〜!!" + "<br/>");
 	} else if (i % 8 === 0) {
 		document.write(i + "オーモロー!!" + "</br>");
