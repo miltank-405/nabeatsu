92edf4fc0526bceda9b07d49cbcfc1e964cff6eb
miltank-405 <koitn.saru@gmail.com>
2021-09-14 13:30:19 +0900

    次やりたいこと書いた

--- nabeatsu.js
+++ nabeatsu.js
@@ -8,6 +8,9 @@ for(var i = 1; i <= 1000; i++) {
 		document.write(i + "アホ〜!!" + "<br/>");
 	} else if (i % 8 === 0 || /[8]/.test(i)) {
 		document.write(i + "オーモロー!!" + "</br>");
+	// かぶったとき"ナベアツ"って表示させたい
+	} else if (i) {
+		document.write();
 	} else {
 		document.write (i + "<br/>");
 	}
